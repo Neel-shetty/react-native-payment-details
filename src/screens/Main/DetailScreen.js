f20c75789e9ad47d9ac0bdc6fd705e744780d32b
Neel <neelnarayanshetty@gmail.com>
2023-01-22 18:17:06 +0530

    location and ip

--- src/screens/Main/DetailScreen.js
+++ src/screens/Main/DetailScreen.js
@@ -1,14 +1,54 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Location from "expo-location";
+import NetInfo from "@react-native-community/netinfo";
+import publicIP from "react-native-public-ip";
 
 const DetailScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+
+    publicIP()
+      .then((ip) => {
+        console.log(ip);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
-    <View>
-      <Text>DetailScreen</Text>
+    <View style={styles.root}>
+      <Text>{text}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default DetailScreen
+export default DetailScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
